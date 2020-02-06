6aade3b10d35a1d016dacfed56797a7473909f1d
Nick Muenchen <nmuench@clemson.edu>
2020-02-06 16:26:04 -0500

    Initial commit

---
+++ coinMarketParse.py
@@ -0,0 +1,44 @@
+import os
+from bs4 import BeautifulSoup
+import pandas as pd
+import glob
+
+
+def parseFile(fileName, df, scrapingTime):
+    print("Parsing", fileName)
+    marketFile = open(fileName, "r")
+    marketParser = BeautifulSoup(marketFile.read(), "html.parser")
+    marketFile.close()
+
+    currencyTableBody = marketParser.find("tbody")
+    allCurrencyRows = currencyTableBody.find_all("tr")
+
+    currencyDict = dict()
+
+
+
+    for currencyRow in allCurrencyRows:
+        currencyName = currencyRow.find("td", {"class" : "cmc-table__cell cmc-table__cell--sticky cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__name"}).find("a", {"class" : "cmc-link"}).text
+        currencyPrice = currencyRow.find("td", {"class" : "cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price"}).find("a", {"class" : "cmc-link"}).text.replace(",", "").replace("$","")
+        currencyMarketCap = currencyRow.find("td", {"class" : "cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__market-cap"}).text.replace(",", "").replace("$", "")
+        currencySupply = currencyRow.find("td", {"class" : "cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__circulating-supply"}).find("div").text.replace(" * ", "")
+        df = df.append({
+            'time' : scrapingTime,
+            'name' : currencyName,
+            'price' : currencyPrice,
+            'marketCap' : currencyMarketCap,
+            'supply' : currencySupply
+        }, ignore_index = True)
+
+    return(df)
+
+if not os.path.exists("parsed_files"):
+    os.mkdir("parsed_files")
+
+df = pd.DataFrame()
+
+#htmlFiles = os.listdir
+for one_file_name in glob.glob("html_files/*.html"):
+    scrapingTime = os.path.basename(one_file_name).replace("coinMarketPage--", "").replace("html", "")
+    df = parseFile(one_file_name, df, scrapingTime)
+    df.to_csv("parsed_files/coinmarketcap_dataset.csv")
---
+++ coinMarketRequest.py
@@ -0,0 +1,36 @@
+import urllib.request
+import os
+import time
+import datetime
+from bs4 import BeautifulSoup
+
+if not os.path.exists("html_files"):
+    os.mkdir("html_files")
+def MakeHourlyRequest():
+    while True:
+        coinMarketUrl = "https://coinmarketcap.com/"
+        coinMarketPage = MakeRequest(coinMarketUrl)
+        time.sleep(10)
+
+def MakeRequest(urlString):
+    fileName = "html_files/coinMarketPage--" + datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S") + ".html"
+    marketPage = urllib.request.urlopen(urlString)
+    with open(fileName, "wb") as marketFile:
+        marketFile.write(marketPage.read())
+    marketFile.close()
+
+MakeHourlyRequest()
+
+# fileName = "html_files/coinMarketPage--20200128163320.html"
+# with open(fileName, "rb") as marketFile:
+#     coinMarketHtml = marketFile.read()
+# marketFile.close()
+#
+# marketParser = BeautifulSoup(coinMarketHtml, "html.parser")
+# tableRows = marketParser.find_all("tr", class_="cmc-table-row")
+#
+# for tableRow in tableRows:
+#     tableCells = tableRow.contents
+#     currencyName = tableCells[1].contents[0].contents[1].text
+#     currencyPrice = tableCells[1]
+#     print(currencyPrice)
